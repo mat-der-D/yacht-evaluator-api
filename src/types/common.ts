881d9653fa88f7a19f688c738d2d593f9118b204
Smooth Pudding <dummy@address>
2025-12-22 09:16:01 +0900

    [feat] Add new types

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,12 @@
 import { z } from 'zod'
-import { scoreSheetSchema } from '../schemas'
+import {
+  categorySchema,
+  fullDiceSchema,
+  partialDiceSchema,
+  scoreSheetSchema,
+} from '../schemas'
 
 export type ScoreSheet = z.infer<typeof scoreSheetSchema>
+export type Category = z.infer<typeof categorySchema>
+export type FullDice = z.infer<typeof fullDiceSchema>
+export type PartialDice = z.infer<typeof partialDiceSchema>
