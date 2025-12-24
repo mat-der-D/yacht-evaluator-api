6d2294791a854b853f1ec75d08be9ded5067f186
Smooth Pudding <dummy@address>
2025-12-24 09:09:45 +0900

    [refactor] Refactor schema for response of evaluate

--- src/schemas/evaluate.ts
+++ src/schemas/evaluate.ts
@@ -7,22 +7,21 @@ export const evaluateRequestSchema = z.object({
   rollCount: z.int().min(1).max(3),
 })
 
+const diceChoiceSchema = z.object({
+  choiceType: z.literal('dice'),
+  diceToHold: z.array(z.int().min(1).max(6)).min(0).max(5),
+  expectedValue: z.number(),
+})
+
+const categoryChoiceSchema = z.object({
+  choiceType: z.literal('category'),
+  category: categorySchema,
+  expectedValue: z.number(),
+})
+
+const choiceSchema = z.union([diceChoiceSchema, categoryChoiceSchema])
+
 export const evaluateResponseSchema = z.object({
-  data: z
-    .array(
-      z.union([
-        z.object({
-          choiceType: z.literal('dice'),
-          diceToHold: z.array(z.int().min(1).max(6)).min(0).max(5),
-          expectation: z.number(),
-        }),
-        z.object({
-          choiceType: z.literal('category'),
-          category: categorySchema,
-          expectation: z.number(),
-        }),
-      ])
-    )
-    .optional(),
+  data: z.array(choiceSchema).optional(),
   error: z.object({ message: z.string() }).optional(),
 })
