5de1e7f71fa23cfe6afa438ba1255e958c276825
Smooth Pudding <dummy@address>
2025-12-24 18:41:04 +0900

    [feat] Update error message

--- src/schemas/common.ts
+++ src/schemas/common.ts
@@ -6,14 +6,17 @@ const createNumberCategoryVariable = (n: number) =>
   z.literal(multiples(n)).nullable()
 
 export const scoreSheetSchema = z.object({
-  ace: createNumberCategoryVariable(1),
-  deuce: createNumberCategoryVariable(2),
-  trey: createNumberCategoryVariable(3),
-  four: createNumberCategoryVariable(4),
-  five: createNumberCategoryVariable(5),
-  six: createNumberCategoryVariable(6),
-  choice: z.int().min(5).max(30).nullable(),
-  fourOfAKind: z.union([z.int().min(5).max(30), z.literal(0)]).nullable(),
+  ace: createNumberCategoryVariable(1).describe('ace'),
+  deuce: createNumberCategoryVariable(2).describe('deuce'),
+  trey: createNumberCategoryVariable(3).describe('trey'),
+  four: createNumberCategoryVariable(4).describe('four'),
+  five: createNumberCategoryVariable(5).describe('five'),
+  six: createNumberCategoryVariable(6).describe('six'),
+  choice: z.int().min(5).max(30).nullable().describe('choice'),
+  fourOfAKind: z
+    .union([z.int().min(5).max(30), z.literal(0)])
+    .nullable()
+    .describe('fourOfAKind'),
   fullHouse: z
     .int()
     .min(0)
@@ -21,10 +24,11 @@ export const scoreSheetSchema = z.object({
     .refine((n) => ![1, 2, 3, 4, 6, 29].includes(n), {
       error: 'Expected a value excluding 1, 2, 3, 4, 6 and 29',
     })
-    .nullable(),
-  smallStraight: z.literal([0, 15]).nullable(),
-  bigStraight: z.literal([0, 30]).nullable(),
-  yacht: z.literal([0, 50]).nullable(),
+    .nullable()
+    .describe('fullHouse'),
+  smallStraight: z.literal([0, 15]).nullable().describe('smallStraight'),
+  bigStraight: z.literal([0, 30]).nullable().describe('bigStraight'),
+  yacht: z.literal([0, 50]).nullable().describe('yacht'),
 })
 
 export const categorySchema = z.enum([
