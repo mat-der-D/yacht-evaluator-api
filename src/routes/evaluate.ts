3a19b3d11cca0c64ab8e854bb9d3e2679a5b63c7
Smooth Pudding <dummy@address>
2025-12-24 09:31:51 +0900

    [feat] Implement router

--- src/routes/evaluate.ts
+++ src/routes/evaluate.ts
@@ -2,20 +2,21 @@ import { Hono } from 'hono'
 import { zValidator } from '@hono/zod-validator'
 import { evaluateRequestSchema } from '../schemas'
 import type { EvaluateResponse } from '../types'
+import { createDiceSetFromFullDice } from '../utilities/types'
+import { createEvaluators, evaluate } from '../utilities/evaluate'
 
 const evaluateRoute = new Hono()
 
-evaluateRoute.post('/', zValidator('json', evaluateRequestSchema), (c) => {
-  // const { scoreSheet, dice, rollCount } = c.req.valid('json')
-  c.req.valid('json') // ダミーのため一旦 validation だけ行う。
+const binaryFilePath = 'data/yacht_exp.bin'
+const evaluators = await createEvaluators(binaryFilePath)
 
-  // ビジネスロジック実装
-  // 合法手を列挙し、それぞれの評価値を計算
+evaluateRoute.post('/', zValidator('json', evaluateRequestSchema), (c) => {
+  const { scoreSheet, dice, rollCount } = c.req.valid('json')
+  const diceSet = createDiceSetFromFullDice(dice)
+  const choices = evaluate(scoreSheet, diceSet, rollCount, evaluators)
 
   const response: EvaluateResponse = {
-    data: [
-      // 合法手のリスト
-    ],
+    data: choices,
   }
   return c.json(response)
 })
